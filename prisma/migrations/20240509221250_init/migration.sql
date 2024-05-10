6ae5b0cdd22d7687932a77bce357f4c967e9b7c3
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-05-10 10:30:00 -0300

    create database with prisma orm

---
+++ prisma/migrations/20240509221250_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Purchase" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "user" TEXT NOT NULL,
+    "detail" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Purchase_user_key" ON "Purchase"("user");
