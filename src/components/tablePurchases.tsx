f05a1a55193357e9558b001d1786429b4da2755c
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-05-10 20:16:42 -0300

    real date from sqlite

--- src/components/tablePurchases.tsx
+++ src/components/tablePurchases.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Table,
   TableBody,
@@ -8,46 +9,16 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-  },
-  {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-  },
-  {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-  },
-];
+import { usePurchases } from "@/context/PurchaseContext";
+import { totalAmount } from "@/lib/utils";
+import { useEffect } from "react";
 
 export default function TablePurchases() {
+  const { purchases, getAllPurchases } = usePurchases();
+
+  useEffect(() => {
+    getAllPurchases();
+  }, []);
   return (
     <>
       <Table>
@@ -60,23 +31,21 @@ export default function TablePurchases() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {invoices.map((invoice) => (
-            <TableRow key={invoice.invoice}>
-              <TableCell className="font-medium">{invoice.invoice}</TableCell>
+          {purchases.map((purchase) => (
+            <TableRow key={purchase.id}>
+              <TableCell className="font-medium">{purchase.user}</TableCell>
               <TableCell className="text-left" colSpan={2}>
-                {invoice.paymentStatus}
+                {purchase.detail}
               </TableCell>
 
-              <TableCell className="text-right">
-                {invoice.totalAmount}
-              </TableCell>
+              <TableCell className="text-right">{purchase.amount}</TableCell>
             </TableRow>
           ))}
         </TableBody>
         <TableFooter>
           <TableRow>
             <TableCell colSpan={4} className="w-full text-right">
-              <span className="text-lg">Total </span> $2,500.00
+              <span className="text-lg">Total </span> {totalAmount(purchases)}
             </TableCell>
           </TableRow>
         </TableFooter>
