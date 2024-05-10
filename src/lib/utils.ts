e524921ad3d13ea3134bebd999aa1f558ed6f0c3
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-05-10 20:14:03 -0300

    totalAmount utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { type ClassValue, clsx } from "clsx";
+import { IPurchase } from "@/app/interfaces/Purchase";
+import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const totalAmount = (purchases: IPurchase[]): number =>
+  purchases.reduce((sum, purchase) => {
+    const amount = purchase.amount;
+    return sum + amount;
+  }, 0);
